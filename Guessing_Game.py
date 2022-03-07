f37801952726b9f299114c9ff2a0ef5b8034f6c8
davtkalec <davtkalec@gmail.com>
2022-03-07 17:07:13 +0100

    Update Guessing_Game.py
    Best score logic updated wit min function. Play again logic changed from if to while loop.

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -7,7 +7,7 @@ def start_game(attempts):
     
     print("---->Welcome to the Guessing game. May the numbers be in your favor!<----\n")
     if len(attempts) > 0:
-        print(f'Best score is {attempts[0]} tries. Can you do better?\n')    
+        print(f'Best score is {min(attempts)} tries. Can you do better?\n')    
     
     random_number = random.randint(1,100)
     random_number = int (random_number) 
@@ -20,7 +20,7 @@ def start_game(attempts):
             if guess > 100 or guess < 1:
                 raise UnboundLocalError("Entered number is out of range")
         except UnboundLocalError as err:
-                print("We ran into an issue. {}. Please try again!".format(err)) ## formatted f-string needed ##
+                print(f'We ran into an issue. {err}. Please try again!') 
                 guess = input("Please enter a number: ")
                 guess = int(guess)
                 counter += 1   
@@ -50,12 +50,10 @@ def start_game(attempts):
     print(f"The average number of tries is: {score_average}\n")
         
     play_again = input("Do you want to play again? y/n ")
-    if play_again == "y":
-            start_game(attempts)
+    while play_again == "y":
+        start_game(attempts)
     else:
-            print("Thank you for taking part in the Guessing game. Have a great day!")
-            exit()
+        print("Thank you for taking part in the Guessing game. Have a great day!")
+        exit()
     
 start_game(attempts)
-
-
